993dcf821160478ff2205190b66e7261198a3ca0
André Felipe <a.n.d.r.e.11.ufcfc@gmail.com>
2026-04-08 20:01:05 -0300

    Update main.dart

--- questao09/main.dart
+++ questao09/main.dart
@@ -1,7 +1,7 @@
 import 'carro.dart';
 
 void main() {
-  Carro carro = Carro('Fusca', 12000);
+  Carro carro = Carro('Honda Civic', 12000);
   Oficina oficina = Oficina();
 
   print('Antes da revisão:');
