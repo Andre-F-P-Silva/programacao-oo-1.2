3d96b5ae2e12f3a2fe6288907b9bf55d17c12456
André Felipe <a.n.d.r.e.11.ufcfc@gmail.com>
2026-04-08 19:46:03 -0300

    Update retangulo.dart

--- questao06/retangulo.dart
+++ questao06/retangulo.dart
@@ -2,15 +2,12 @@ class Retangulo {
   double _largura;
   double _altura;
 
-  // Construtor padrão com parâmetros
   Retangulo(this._largura, this._altura);
 
-  // Construtor sem parâmetros: cria um quadrado 1x1
   Retangulo.padrao()
       : _largura = 1,
         _altura = 1;
 
-  // Construtor nomeado: cria um quadrado com o lado informado
   Retangulo.quadrado(double lado)
       : _largura = lado,
         _altura = lado;
