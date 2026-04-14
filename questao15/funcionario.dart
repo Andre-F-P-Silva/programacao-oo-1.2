7bb9351378dcc244b1fc2a0a56fecc7ff321e00c
André Felipe <a.n.d.r.e.11.ufcfc@gmail.com>
2026-04-13 21:53:10 -0300

    Update funcionario.dart

--- questao15/funcionario.dart
+++ questao15/funcionario.dart
@@ -4,21 +4,18 @@ class Funcionario {
   double _salario;
   double _bonus;
 
-  // Construtor estagiário: cargo e salário já definidos automaticamente
   Funcionario.estagiario(String nome)
       : _nome = nome,
         _cargo = 'Estagiário',
         _salario = 800,
         _bonus = 0;
 
-  // Construtor pleno: cargo definido, salário informado
   Funcionario.pleno(String nome, double salario)
       : _nome = nome,
         _cargo = 'Pleno',
         _salario = salario,
         _bonus = 0;
 
-  // Construtor gerente: parâmetros nomeados, bônus tem valor padrão de 1000
   Funcionario.gerente({required String nome, double bonus = 1000})
       : _nome = nome,
         _cargo = 'Gerente',
